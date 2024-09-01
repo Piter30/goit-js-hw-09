4f4eb1436d604fe3254f0482de90c48b7e62a001
Piter30 <ditq65@gmail.com>
2024-09-01 17:01:26 +0200

    next routing fix

--- src/js/routing.js
+++ src/js/routing.js
@@ -1,8 +1,19 @@
-const repoName = '/goit-js-hw-09';
-const navLinks = document.querySelectorAll('.nav-link');
+document.addEventListener('DOMContentLoaded', () => {
+  const repoName = '/goit-js-hw-09';
+  const navLinks = document.querySelectorAll('.nav-link');
 
-navLinks.forEach(link => {
-  console.log(`Original href: ${link.getAttribute('href')}`); // Logowanie oryginalnego href
-  link.href = repoName + '/' + link.getAttribute('href');
-  console.log(`Updated href: ${link.href}`); // Logowanie zaktualizowanego href
+  const isGitHubPages = window.location.hostname.includes('github.io');
+
+  if (isGitHubPages) {
+    navLinks.forEach(link => {
+      let href = link.getAttribute('href');
+
+      href = href.replace(/^(\.\/|\.\.\/)+/, '');
+
+      link.href = `${repoName}/${href}`;
+
+      console.log(`Original href: ${link.getAttribute('href')}`);
+      console.log(`Updated href: ${link.href}`);
+    });
+  }
 });
