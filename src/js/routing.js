5dbd750641a41bd89d1349f7197f4e30706d71ca
Piter30 <ditq65@gmail.com>
2024-09-01 16:56:04 +0200

    fix

--- src/js/routing.js
+++ src/js/routing.js
@@ -2,5 +2,7 @@ const repoName = '/goit-js-hw-09';
 const navLinks = document.querySelectorAll('.nav-link');
 
 navLinks.forEach(link => {
-  link.href = repoName + link.getAttribute('href');
+  console.log(`Original href: ${link.getAttribute('href')}`); // Logowanie oryginalnego href
+  link.href = repoName + '/' + link.getAttribute('href');
+  console.log(`Updated href: ${link.href}`); // Logowanie zaktualizowanego href
 });
