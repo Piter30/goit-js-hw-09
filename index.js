de2fd6889aebb1ce4fb42772b3cad05de4dd2f41
Piter30 <48550262+Piter30@users.noreply.github.com>
2024-09-01 14:50:57 +0000

    Deploying to gh-pages from @ Piter30/goit-js-hw-09@04b48ffc0727e1a955d5dda43ab4fd5bd03d0e79 🚀

--- index.js
+++
@@ -1,2 +0,0 @@
-import"./assets/styles-BkHRcmk2.js";const t="/goit-js-hw-09",o=document.querySelectorAll(".nav-link");o.forEach(e=>{e.href=t+e.getAttribute("href")});
-//# sourceMappingURL=index.js.map
